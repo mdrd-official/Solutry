39a2b39b6b2e7c95e0da39606e2b072aa66a97ff
MUHAMMED RASHID <mr0248974@gmail.com>
2023-07-28 23:32:45 +0530

    Initial Commit

---
+++ assets/js/script.js
@@ -0,0 +1,41 @@
+'use strict';
+
+/**
+ * navbar toggle
+ */
+const header = document.querySelector("[data-header]");
+const navToggleBtn = document.querySelector("[data-nav-toggle-btn]");
+const navLinks = document.querySelectorAll("[data-navbar-link]");
+
+navToggleBtn.addEventListener("click", function () {
+  this.classList.toggle("active");
+  header.classList.toggle("nav-active");
+});
+
+for (let i = 0; i < navLinks.length; i++) {
+  navLinks[i].addEventListener("click", function () {
+    header.classList.remove("nav-active"); // Close the navigation menu
+    navToggleBtn.classList.remove("active"); // Remove the "active" class from the toggle button
+  });
+}
+
+
+
+
+/**
+ * header scroll active state & go to top
+ */
+
+const goTopBtn = document.querySelector("[data-go-top]");
+
+window.addEventListener("scroll", function () {
+
+  if (window.scrollY >= 100) {
+    header.classList.add("active");
+    goTopBtn.classList.add("active");
+  } else {
+    header.classList.remove("active");
+    goTopBtn.classList.remove("active");
+  }
+
+});
